0ae576bdc9ff10d67b06f89a5a626c3e1fa7877a
Jeroen Vermeulen <info@jeroenvermeulen.eu>
2015-04-02 14:33:05 +0200

    Version 1.10.3: Improved URL flush again

--- app/code/community/JeroenVermeulen/BlockCache/controllers/Adminhtml/Jv/BlockcacheController.php
+++ app/code/community/JeroenVermeulen/BlockCache/controllers/Adminhtml/Jv/BlockcacheController.php
@@ -46,7 +46,13 @@ class JeroenVermeulen_BlockCache_Adminhtml_Jv_BlockcacheController extends Mage_
             $httpClient->setConfig( array('httpversion' => Zend_Http_Client::HTTP_0) );
             $response = $httpClient->request();
 
-            $sLink = sprintf( '<a href="%s" target="_blank">%s</a>', $url, $url );
+            $freshUrl = $url;
+            if ( false === strpos($freshUrl,'?') ) {
+                $freshUrl .= '?'.time();
+            } else {
+                $freshUrl .= '&'.time();
+            }
+            $sLink = sprintf( '<a href="%s" target="_blank">%s</a>', $freshUrl, $url );
 
             if ( $response->isSuccessful() ) {
                 $msg = $this->__( 'The URL %s has been flushed.', $sLink );
